170ee491cfe9604889392bd56c5d61bc4a6a474b
MeganeKickouama <%kickouama.megane@gmail.com%>
2023-11-08 20:15:36 -0500

    tried to do nav bar. will continue later

---
+++ navbar.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+    <script defer src="friconix.js"> </script>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <div>
+            <table>
+                <tr>
+                    <form action="">
+                    <td>
+                        <h2> <a href="index.php">SHMewelry</a> </h2>
+                    </td>
+                    <td id="searchQuery">
+                        <input type="text" name="searchQuery" placeholder="search">
+                        <button type="submit" class="searchIcon"><i class="fa fa-search"></i></button>
+                    </td>
+                    <td>
+                        <button name="cartButton"><i class="fa fa-shopping-cart"></i></button>
+                    </td>
+                    <td>
+                        <button name="profileButton" id="profileButton"><i class="fa fa-user-circle"></i></button>
+
+                    </td>
+                    </form>
+                </tr>
+            </table>
+        </div>
+    </body>
+</html>
