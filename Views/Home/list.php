dd87f5f48d1280d6c44f56997c440c80036d45f7
%hqara% <%hibba_qaraman@hotmail.com%>
2023-11-29 15:18:54 -0500

    navbar and css

--- Views/Home/list.php
+++ Views/Home/list.php
@@ -11,25 +11,28 @@
     <!-- From Bootdey.com -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-    <?php
-        include_once __DIR__ . "/../../navbar.php";
-    ?>
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
     <link href="product_listing.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script type="text/javascript"></script>
+    <link href="shared.css" rel="stylesheet">
+</head>
+
+<body>
+</header>
+    <?php include_once __DIR__ . "/../../navbar.php"; ?>
+</header>
+
     <div class="container">
         <?php
             Product::buildProductPage();
         ?>
     </div>
-    <?php
-        include_once __DIR__ . "/../../footer.html";
-    ?>
-    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <script type="text/javascript"></script>
-    <link href="shared.css" rel="stylesheet">
+    <footer>
+        <?php include_once __DIR__ . "/../../footer.html"; ?>
+    </footer>
+
 </body>
 
 </html>
--- navbar.php
+++ navbar.php
@@ -8,195 +8,201 @@ $isLoggedIn = isset($_SESSION['user_id']) && !empty($_SESSION['user_id']);
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>SHMewelry</title>
+    <link rel="stylesheet" href="path/to/font-awesome.min.css">
 
     <style>
-        html, body {
-            height: 100%;
-            margin: 0;
-        }
+   html, body {
+    height: 100%;
+    margin: 0;
+}
 
-        body {
-            display: flex;
-            flex-direction: column;
-        }
+body {
+    display: flex;
+    flex-direction: column;
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #fff;
+    overflow-x: hidden;
+}
 
-        header, footer {
-            flex-shrink: 0; 
-        }
+header {
+    background-color: #fff;
+    color: #333;
+    padding: 10px;
+}
 
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #fff;
-        }
+.header-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
 
-        header {
-            background-color: #fff;
-            color: #333;
-            padding: 10px;
-        }
+.left-box {
+    color: #ffffff;
+    display: flex;
+    align-items: center;
+}
 
-        a {
-            text-decoration: no-underline;
-        }
+.right-box {
+    padding-left: 100px;
+}
 
-        nav, nav .dropdown {
-            background-color: #fff;
-            padding: 10px;
-            text-align: center;
-            position: initial;
-            z-index: 100;
-        }
+.right-box table {
+    margin-left: auto;
+}
 
-        nav a {
-            margin-right: 20px;
-            text-decoration: none;
-            color: #808080;
-            font-weight: bold;
-            transition: color 0.3s;
-        }
+.right-box input {
+    margin-right: 10px;
+}
 
-        nav a:hover {
-            color: #6ac5fe;
-            position: relative;
-            z-index: 1;
-        }
+.dropdown {
+    position: relative;
+    display: table-cell;
+}
 
-        section {
-            padding: 20px;
-        }
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #fff;
+    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+}
 
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
+.dropdown .profile-dropdown{
+    position: relative;
+    z-index: 1;
+}
 
-        th, td {
-            padding: 10px;
-        }
+.dropdown:hover .dropdown-content {
+    display: block;
+    z-index: 100;
+}
 
-        .wider-column {
-            width: 80%;
-        }
+.dropdown-content a {
+    display: block;
+    padding: 10px;
+    text-decoration: none;
+    color: #808080;
+    font-weight: bold;
+    transition: color 0.3s;
+}
 
-        .wider-column2 {
-            width: 1050px;
-        }
-
-        section {
-            background-color: #6ac5fe;
-            padding: 3px;
-        }
-
-        a {
-            text-decoration: none;
-            color: black;
-        }
-
-        a:active, a:focus {
-            color: black;
-        }
-
+.dropdown-content a:hover {
+    color: #6ac5fe;
+    transition: color 0.3s;
     
-        .dropdown, .dropdown button {
-            position: relative;
-            z-index: 1;
-        }
+}
 
+a {
+    text-decoration: none;
+    color: black;
+    padding: 10px;
+    text-align: center;
+}
 
-        .dropdown-content, button .dropdown-content {
-            top: 100%; 
-            display: none;
-            position: absolute;
-            background-color: #fff;
-            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-            min-width: 160px; 
-            z-index: 2;
-        }
+nav {
+    max-width: 100%;
+}
 
-        .dropdown:hover .dropdown-content {
-            display: block;
-        }
+nav a {
+    text-decoration: none;
+    color: #808080;
+    font-weight: bold;
+    transition: color 0.3s;
+    padding: 10px;
+    display: inline-block;
+}
 
-        .dropdown-content a {
-            display: block;
-            width: 100%;
-            padding: 10px;
-            text-decoration: none;
-            margin-right: 20px;
-            color: #808080;
-            font-weight: bold;
-            transition: color 0.3s;
-        }
+nav a:hover {
+    color: #6ac5fe;
+}
 
-        .dropdown-content a:hover {
-            color: #6ac5fe;
-            position: relative;
-            transition: color 0.3s;
-        }
+section {
+    padding: 20px;
+}
+
+table {
+    border-collapse: collapse;
+    width: 100%;
+}
+
+table td {
+    max-width: 100%;
+    flex-direction: column;
+    align-items: center;
+}
+
+th, td {
+    padding: 10px;
+}
+
+.wider-column {
+    width: 80%;
+}
+
+.wider-column2 {
+    width: 92%;
+}
+
+section {
+    background-color: #6ac5fe;
+    padding: 3px;
+}
 
 
     </style>
-
 </head>
 
 <body>
-
     <header>
-        <div>
-            <table>
-                <tr>
-                    <td class="wider-column">
-                        <h1><a href="index.php?controller=home&action=index">SHMewelry</a></h1>
-                    </td>
-
-                    <td>
-                        <input type="text" name="searchQuery" placeholder="Search">
-                        <button type="submit" class="searchIcon"><i class="fa fa-search"></i></button>
-                        <button name="cartButton" <?php echo $isLoggedIn ? '' : 'disabled'; ?>><i class="fa fa-shopping-cart"></i></button>
-                    
-
-                    <div class="dropdown">
+        <div class="header-container">
+            <div class="left-box">
+                <h1><a href="index.php?controller=home&action=index">SHMewelry</a></h1>
+            </div>
+            <div class="right-box">
+                <table>
+                    <tr>
+                        <td>
+                            <input type="text" name="searchQuery" placeholder="Search">
+                        </td>
+                        <td>
+                            <button type="submit" class="searchIcon"><i class="fa fa-search"></i></button>
+                        </td>
+                        <td>
+                            <button name="cartButton" <?php echo $isLoggedIn ? '' : 'disabled'; ?>><i class="fa fa-shopping-cart"></i></button>
+                        </td>
+                        <td class="dropdown profile-dropdown">
                             <button name="profileButton" id="profileButton" <?php echo $isLoggedIn ? 'onclick="redirectToProfile()"' : 'onclick="openLoginPage()"'; ?>>
                                 <i class="fa fa-user-circle"></i>
                             </button>
-                    
-                        <script>
-                            function redirectToProfile() {
-                                // Replace 'profile.php' with the actual path to the user profile page
-                                window.location.href = '../../../../SHMewelry/Views/Login/login.php';
-                                //window.location.href = 'index.php?controller=home&action=login'; 
-                            }
+                            <script>
+                                function redirectToProfile() {
+                                    window.location.href = '../../../../SHMewelry/Views/Login/login.php';
+                                }
 
-                            function openLoginPage() {
-                                console.log('Opening login page');
-                                // Replace 'login.php' with the actual path to the login page
-                                window.location.href = 'index.php?controller=login&view=login';
-                                //window.location.href = 'index.php?controller=home&action=login';  //   MODIFY
-                            }
-                        </script>
-                        <div class="dropdown-content">
-                            <a href="index.php?controller=user&action=read">My Profile</a>
-                            <a href="index.php?controller=orders&action=list">Manage Orders/My Orders</a>
-                            <a href="index.php?controller=product&action=list">Manage Products</a>
-                            <a href="index.php?controller=user&action=list">Manage Users and Permissions</a>
-                            <a href="index.php?controller=user&action=exit">Logout</a>
-                        </div>
-                        </div>
-                    </td>
-                </tr>
-            </table>
+                                function openLoginPage() {
+                                    console.log('Opening login page');
+                                    window.location.href = 'index.php?controller=login&view=login';
+                                }
+                            </script>
+                            <div class="dropdown-content">
+                                <a class="profile" href="index.php?controller=user&action=read">My Profile</a>
+                                <a class="profile" href="index.php?controller=orders&action=list">Manage Orders/My Orders</a>
+                                <a class="profile" href="index.php?controller=product&action=list">Manage Products</a>
+                                <a class="profile" href="index.php?controller=user&action=list">Manage Users and Permissions</a>
+                                <a class="profile" href="index.php?controller=user&action=exit">Logout</a>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
     </header>
-
-    <nav>
-        <table>
+<nav>
+<table>
             <tr>
                 <td><a href="index.php?controller=home&view=index">Home</a></td>
                 <td class="dropdown">
@@ -279,11 +285,11 @@ $isLoggedIn = isset($_SESSION['user_id']) && !empty($_SESSION['user_id']);
         </table>
     </nav>
 
-    <section>
 
-    </section>
-    <br>
-    <br>
+<section>
+
+</section>
+<br>
+<br>
 </body>
-
 </html>
